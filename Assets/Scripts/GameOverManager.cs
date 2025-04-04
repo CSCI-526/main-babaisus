e1eb9243163633305d3b52e34f03d2d856ce1ac4
ChessyWang <ywang150@usc.edu>
2025-04-04 11:52:50 +0800

    fix: change time text

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -36,7 +36,7 @@ public class GameOverManager : MonoBehaviour
         gamePassUI.SetActive(true);
 
         var passTime = timer.GetSpentTime();
-        timeText.text = $"Time spent before ball drop: {passTime} seconds";
+        timeText.text = $"Seconds before ball drop: {passTime}";
 
         var tutorialCompleted = LevelSelectionManager.type == 1 && LevelSelectionManager.currentLevel == 4;
         var mainCompleted = LevelSelectionManager.type == 0 && LevelSelectionManager.currentLevel == 21;
