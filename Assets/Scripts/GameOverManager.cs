aa90c585ff728797ba9150c067175a3cb10029d5
seeratparmar <seeratparmar@gmail.com>
2025-04-10 23:34:51 -0700

    Update GameOverManager.cs

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -36,7 +36,7 @@ public class GameOverManager : MonoBehaviour
         gamePassUI.SetActive(true);
 
         var passTime = timer.GetSpentTime();
-        timeText.text = $"Spent {passTime}s before Ball Drop";
+        timeText.text = $"Spent {passTime}s before ball dropped";
 
         var tutorialCompleted = LevelSelectionManager.type == 1 && LevelSelectionManager.currentLevel == 4;
         var mainCompleted = LevelSelectionManager.type == 0 && LevelSelectionManager.currentLevel == 21;
